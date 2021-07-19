551c60c222054c15c2bfa10352b2156fa10b97da
Alan <alanrob7@gmail.com>
2021-07-18 20:32:16 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre:string="fernando hErrera";
+  valor:number=1100;
+
+  cambiarNombre(){
+    console.log(this.nombre);
+    console.log(this.valor);
+  }
 }
